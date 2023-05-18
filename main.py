f53be1c95dfdfc56b2a22114da99f85680ba569b
Adrian Gruenberg <adriang2000@gmx.de>
2023-05-18 12:54:06 +0200

    added environment variables

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ from association_manager import checkAssociations
 from serial_connector import scan_RFID_tags
 from pydub import AudioSegment
 from pydub.playback import play
+import os
+import sys
 
 def main():
-    serial_connection = serial.Serial('/dev/cu.usbserial-210', 9600, timeout=0.01)
+    serial_connection = serial.Serial(os.environ.get("SERIAL_PORT_PATH"), 9600, timeout=0.01)
     success_audio = AudioSegment.from_file("./sound/ringtone-1-46486.mp3")
     error_audio = AudioSegment.from_file("./sound/melancholy-ui-chime.mp3")
     
@@ -27,4 +29,25 @@ def main():
     keyboard.wait()
 
 if __name__ == "__main__":
+    # Set environment variables depending on target platform
+    args = sys.argv[1:]
+    if not len(args) >= 1:
+        print("Insufficient arguments provided. Please specify the environment with --mac or --pi.")
+        sys.exit(1)
+    
+    env_file_path = ""
+    if args[0] == "--pi":
+        env_file_path = "env.raspberryPi.txt"
+    if args[0] == "--mac":
+        env_file_path = "env.macintosh.txt"
+
+    with open(env_file_path, 'r') as file:
+        lines = file.readlines()
+        for line in lines:
+            line = line.strip()  # Remove leading/trailing whitespace
+            if line:
+                var_name, var_value = line.split('=')
+                os.environ[var_name] = var_value
+                print(var_name, var_value)
+
     main()
